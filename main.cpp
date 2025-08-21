a19b90269d91d28de67798f5e244305da6a94a33
Felipe Viana <felipetiburcioviana@gmail.com>
2025-08-21 14:40:03 -0700

    Added more basic material

--- main.cpp
+++ main.cpp
@@ -30,9 +30,9 @@ using namespace  std;
 int main ()
 {
 
-    int file_size = 0;
-    int counter = 0;
-    double sales = 9.99;
+    int file_size   = 0;
+    int counter     = 0;
+    double sales    = 9.99;
 
     std::cout <<  file_size;
 
